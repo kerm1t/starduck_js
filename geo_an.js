3c6d1878cc7425d96568ef047a40f793f9d22c55
kerm1t <wolfgang.schulz@gmail.com>
2023-08-19 15:19:07 +0300

    lowr now rel. to upper, but anim. still wrong->NOT

--- geo_an.js
+++ geo_an.js
@@ -30,7 +30,7 @@ function bresenham(x0, y0, x1, y1, col) // https://de.wikipedia.org/wiki/Bresenh
 
     while (true)
     {
-        buf[y0*600+x0]=col;
+      if ((x0 >= 0) && (x0<600)) buf[y0*600+x0]=col;
 
         if (x0==x1 && y0==y1) break;
         e2 = 2*err;
@@ -47,7 +47,7 @@ function bresenham_arr(x0, y0, x1, y1, xarr, col) // https://de.wikipedia.org/wi
 
   while (true)
   {
-    buf[y0*600+x0]=col;
+    if ((x0 >= 0) && (x0<600)) buf[y0*600+x0]=col;
     xarr[y0] = x0; // <-- wichtig! x-pos merken!
 
     if (x0 == x1 && y0 == y1) break;
@@ -105,19 +105,19 @@ function triangle_filled(x0,y0,x1,y1,x2,y2, col)
   }
 }
 
-
+y_top = 0;
+y_h = 600;
 const max_bars = 30
-bars = 8; // multiple of 2
-barwdth = 10;
+bars = 16; // multiple of 2
+barwdth = 80;
+barshalf = bars*barwdth/2;
 const piano_init = new Array(max_bars+1);
-const piano_init_low = new Array(max_bars+1);
+// piano_low determined by upper
 const piano_col = new Array(max_bars+1)
 var piano_up = new Array(max_bars+1);
-var piano_low = new Array(max_bars+1);
 for (i=0;i<=bars;i++)
 {
   piano_init[i] = i*barwdth;
-  piano_init_low[i] = -30+i*barwdth+5;
   if (i%2==1) piano_col[i] = reverseUint32(0x0000ffff); else piano_col[i] = reverseUint32(0xff00ffff);
 }
 bufidx = 0;
@@ -125,10 +125,11 @@ var x1 = new Array(max_bars+1);
 var x2 = new Array(max_bars+1);
 
 function draw_bar(x1,x2,x3,x4,col) {
-  var p0 = vec2(x1,10);
-  var p1 = vec2(x2,10);
-  var p2 = vec2(x4,100); // low
-  var p3 = vec2(x3,100); // low
+  var shiftme = 0; // barshalf;
+  var p0 = vec2(shiftme+x1,y_top);
+  var p1 = vec2(shiftme+x2,y_top);
+  var p2 = vec2(shiftme+x4,y_h); // low
+  var p3 = vec2(shiftme+x3,y_h); // low
   buf[p0.y*600+p0.x]=col;
   buf[p1.y*600+p1.x]=col;
   buf[p2.y*600+p2.x]=col;
@@ -136,15 +137,16 @@ function draw_bar(x1,x2,x3,x4,col) {
   triangle_filled(p0.x,p0.y, p1.x,p1.y,p2.x,p2.y, col); // r
   triangle_filled(p2.x,p2.y, p3.x,p3.y,p0.x,p0.y, col); // r
 }
-var chg = 0;
+
+var sl = 0;
 setInterval(function(){
   var t_start=Date.now();
   
-  sl = parseInt(slider.value);
+//  sl = parseInt(slider.value)*5; // manual
+  sl+=10; // auto
   var start = 0;
   for (j=0;j<=bars;j++) {
     piano_up[j]=(piano_init[j]+sl) % (bars*barwdth+1);
-    piano_low[j]=(piano_init_low[j]+sl*2) % (bars*barwdth+5+1);
     if (piano_up[j] < piano_up[start]) start = j; // start with smallest x-value
   }
 
@@ -152,8 +154,9 @@ setInterval(function(){
     j = start+i;
     x1 = piano_up[j%(bars+1)];
     x2 = piano_up[(j+1)%(bars+1)];
-    x3 = piano_low[j%(bars+1)];
-    x4 = piano_low[(j+1)%(bars+1)];
+// lower need to be relative to upper
+    x3 = 2*piano_up[j%(bars+1)]-barshalf;
+    x4 = 2*piano_up[(j+1)%(bars+1)]-barshalf;
     col = piano_col[j%(bars+1)];
     if (x2 > (bars*barwdth+1)) {
       xtmp = (bars*barwdth+1);
@@ -167,8 +170,8 @@ setInterval(function(){
 
   t.innerText="Frame time:"+(Date.now()-t_start)+"ms";
   for (j=0;j<=bars;j++) {
-    ctx.fillText(piano_up[j], 100, 100+j*12);
+    ctx.fillText(piano_up[j], 10, 30+j*12);
   }
-  ctx.fillText("start: "+start, 100, 88);
+  ctx.fillText("start: "+start, 10, 18);
 
   },20);
